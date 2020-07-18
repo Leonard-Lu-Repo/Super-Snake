0e24e60db4f39537304b99c4a0fbd7859c634e32
路兴龙 <luxinglong@luxinglongdeMacBook-Air.local>
2020-07-18 18:19:45 -0400

    Delete add snake class

--- Scripts/managers/AddSnake.ts
+++
@@ -1,7 +0,0 @@
-module managers{
-    export class AddSnake{
-        public static addSnakeBody(assetManager:createjs.LoadQueue, imageString:string,preSnakebody:objects.SnakeBody,posSnakebody:objects.SnakeBody){
-            
-        }
-    }
-}
